5f59691b0223a26a2c70a38db35479e67f18a5e7
AhmedSHG97 <ahmed2012ramadan@gmail.com>
2022-09-22 13:59:53 +0200

    check if the user have the ability to use the luck wheel

--- routes/api/v1/user.php
+++ routes/api/v1/user.php
@@ -10,7 +10,9 @@
 |
  */
 use App\Base\Constants\Auth\Role;
-
+use App\Models\Luck;
+use Carbon\Carbon;
+use Illuminate\Routing\ResponseFactory;
 /*
  * These routes are prefixed with 'api/v1/user'.
  * These routes use the root namespace 'App\Http\Controllers\Api\V1\User'.
@@ -19,6 +21,51 @@ use App\Base\Constants\Auth\Role;
 Route::prefix('user')->namespace('User')->middleware('auth')->group(function () {
     // Get the logged in user.
     Route::get('/', 'AccountController@me');
+    Route::get('luckWheel', function(ResponseFactory $response){
+        $user_id = auth()->user()->id;
+        $user_count = Luck::where('status',0)->where("user_id",$user_id)->whereDay("updated_at",Carbon::now()->format('d'))->count();
+        if($user_count == 0){
+            $data =  Luck::where('status',1)->inRandomOrder()->first();
+            if($data){
+                $data->update(['status' => 0, 'updated_at' => now(), 'user_id' => $user_id]);
+                $data->refresh();
+            }
+            $body = [
+                "status" => true,
+                "message" => "data sent successfully",
+                "data" => $data
+            ];
+            
+            return $response->json($body,200, [], JSON_NUMERIC_CHECK);
+        }
+        $body = [
+            "status" => true,
+            "message" => "you can only use this once",
+            "data" => null
+        ];
+        return $response->json($body,200, [], JSON_NUMERIC_CHECK);
+       
+    });
+    Route::get('checkLuckWheel', function(ResponseFactory $response){
+        $user_id = auth()->user()->id;
+        $user_count = Luck::where('status',0)->where("user_id",$user_id)->whereDay("updated_at",Carbon::now()->format('d'))->count();
+        if($user_count == 0){
+            $body = [
+                "status" => true,
+                "message" => "wheel is available",
+                "data" => null
+            ];
+            
+            return $response->json($body,200, [], JSON_NUMERIC_CHECK);
+        }
+        $body = [
+            "status" => false,
+            "message" => "wheel is not available for that user know",
+            "data" => null
+        ];
+        return $response->json($body,200, [], JSON_NUMERIC_CHECK);
+       
+    });
     /**
      * These routes use the middleware group 'role'.
      * These routes are accessible only by a user with the 'user' role.
